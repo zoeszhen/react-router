fb265aab9036cd719e8f19b5448d856354d492ab
SHI ZHEN <zoe.zhenshi@gmail.com>
2017-08-12 16:01:06 +0300

    First commit

---
+++ src/components/Courses.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {NavLink, BrowserRouter, Route, Redirect} from "react-router-dom";
+import CourseContainer from "./courses/CourseContainer";
+import { HTMLCourses } from '../data/courses';
+import { CSSCourses } from '../data/courses';
+import { JSCourses } from '../data/courses';
+
+const Courses = ({match}) => (
+
+  
+  <div className="main-content courses">
+    <div className="course-header group">
+      <h2>Courses</h2> 
+    </div>
+    <BrowserRouter>
+      <div className="container">
+        <ul className="course-nav">
+          <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
+          <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+          <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
+        </ul>
+        
+        <Route exact path={match.url} render={ ()=><Redirect to={`${match.url}/html`}/>} /> 
+        <Route  path={`${match.url}/html`} render={()=><CourseContainer data={HTMLCourses} />}/>
+        <Route  path={`${match.url}/css`} render={()=><CourseContainer data={CSSCourses}/>}/>
+        <Route  path={`${match.url}/javascript`} render={()=><CourseContainer data={JSCourses}/>} />
+      </div>
+      </BrowserRouter>
+    {/* Write routes here... */}
+  </div>
+);
+
+export default Courses;
