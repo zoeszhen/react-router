fc3d786fa143ed2444b0ea97826efd7c91136f27
SHI ZHEN <zoe.zhenshi@gmail.com>
2017-08-21 19:36:22 +0300

    - error simulation

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {
   BrowserRouter,
   Route,
@@ -12,21 +12,46 @@ import Courses from "./Courses";
 import Teachers from "./Teachers";
 import NotFound from "./NotFound";
 import Featured from "./Featured";
+import axios from "axios";
 
-const App = () => (
-  <BrowserRouter>
-    <div className="container">
-      <Header/>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={()=><About title="About"/>}/>
-        <Route exact path="/courses" component={Courses} />
-        <Route exact path="/teachers" component={Teachers} />
-        <Route path="/teachers/:topic/:name" component={Featured} />
-        <Route  component={NotFound} />
-      </Switch>
-    </div>
-  </ BrowserRouter>
-);
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      testData:null
+    }
+  }
+
+  componentDidMount=()=>{
+    axios.get('http://mf-rest-apiapp.azurewebsites.net/api/document')
+    .then((res) => {
+      console.log("res",res);
+      this.setState({
+        testData: res.data
+      })
+    })
+    .catch((error)=> {
+      console.log("error",error);
+    })
+  }
+
+  render(){
+    return(
+      <BrowserRouter>
+      <div className="container">
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={()=><About title="About"/>}/>
+          <Route exact path="/courses" component={Courses} />
+          <Route exact path="/teachers" component={Teachers} />
+          <Route path="/teachers/:topic/:name" component={Featured} />
+          <Route  component={NotFound} />
+        </Switch>
+      </div>
+    </ BrowserRouter>
+    )
+  }
+};
 
 export default App;
